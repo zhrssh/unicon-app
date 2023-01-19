c7ee222a730cc0d33ac276ed16e612e043427d7e
jjjjjjjjjj24 <jericodlsrys@gmail.com>
2023-01-19 17:30:22 +0800

    Notifications - test - needs testing

---
+++ src/controller/notificationController.js
@@ -0,0 +1,25 @@
+// communicates with database
+/*
+/**
+ * Creates a new profile and store in the database
+ * @param {*} message
+ * @returns Name
+
+function print(message) { return name}
+
+print()
+*/
+
+/*
+function or async function
+await - halt on findOne if resolve or reject 
+*/
+
+await add()
+
+function add(x, y) {
+    return new Promise((resolve, reject) => {
+        //fromRemote = addchuchunes..
+        resolve(value)
+    })
+}
--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -10,6 +10,23 @@ router.use((req, res, next) => {
 
 router.get('/', (req, res) => {
     // Starting point of the /notifications route
+    res.sendStatus(200)
 })
+
+router.get("/test", (req, res) => {
+    res.sendStatus(302)
+})
+
+
+// time received of notification
+router.get('/notif', (req, res) => {
+    const currentTime = new Date();
+    res.send(`{currentTime}`);
+    })
+      
+router.listen(3000, () => {
+    console.log('Server started on port 3000');
+      })
+
 
 module.exports = router
