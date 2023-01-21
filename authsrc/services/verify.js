800a5b163c8ab1aa9f2402a3e09e66496da588dc
zhrssh <98146850+zhrssh@users.noreply.github.com>
2023-01-21 20:48:01 +0800

    Refactored registration and verification codes

--- authsrc/services/verify.js
+++ authsrc/services/verify.js
@@ -1,7 +1,5 @@
 require("dotenv").config()
 
-const path = require("path")
-
 const User = require("../model/Users")
 const generateKey = require("../utils/generate").generateKey
 const send = require("./email").send
@@ -9,123 +7,148 @@ const send = require("./email").send
 /**
  * Sets the user to "Active", indicating the email is verified. 
  * Sends another email telling the user is verified.
- * @param {*} confirmationCode 
+ * @param {string} uuid
+ * @param {string} confirmationCode 
+ * @returns {Promise<null>}
  */
-async function verifyUser(uuid, confirmationCode) {
-    const user = await User.findOne({ _id: uuid })
+function verifyUser(uuid, confirmationCode) {
+    return new Promise(async (resolve, reject) => {
+        const user = await User.findOne({ _id: uuid })
 
-    // Checks if the user with the confirmation code exists
-    if (user == null) {
-        const error = new Error("User does not exist.")
-        error.code = "404"
-        throw error
-    }
+        // Checks if the user with the confirmation code exists
+        if (user == null) {
+            if (process.env.NODE_ENV == "development")
+                console.log(getDate(Date.now()), "User does not exists.")
+            return reject(raise("E03", 404))
+        }
 
-    // Checks if the correct code is sent
-    if (user.confirmationCode !== confirmationCode) {
-        const error = new Error("Incorrect confirmation code.")
-        error.code = "401"
-        throw error
-    }
+        // Checks if the correct code is sent
+        if (user.confirmationCode !== confirmationCode) {
+            if (process.env.NODE_ENV == "development")
+                console.log(getDate(Date.now()), "Incorrect confirmation code.")
+            return reject(raise("E01", 401))
 
-    // Sets the user status to "Active"
-    try {
+        }
+
+        // Sets the user status to "Active"
         user.status = "Active"
-        await user.save()
-    } catch (err) {
-        const error = new Error(err.message)
-        error.code = "500"
-        throw error
-    }
+        user.save((err, result) => {
+            if (err) {
+                if (process.env.NODE_ENV == "development")
+                    console.log(getDate(Date.now()), err.message)
+                return reject("S01", 500)
+            }
+        })
 
-    // Send success email to the user
-    let dir = "/../templates/success.html"
+        // Send success email to the user
+        let dir = "/../templates/success.html"
 
-    // Email config
-    let config = {
-        from: process.env.EMAIL,
-        to: user.email,
-        subject: "RE: Account Verification Successful"
-    }
+        // Email config
+        let config = {
+            from: process.env.EMAIL,
+            to: user.email,
+            subject: "RE: Account Verification Successful"
+        }
 
-    // Replacements in the html
-    let replacements = {
-        email: user.email,
-        emailSupport: "support@uniconapp.com"
-    }
+        // Replacements in the html
+        let replacements = {
+            email: user.email,
+            emailSupport: "support@uniconapp.com"
+        }
 
-    // Sends the email
-    send(config.from, config.to, config.subject, dir, replacements)
+        // Send email
+        await send(config.from, config.to, config.subject, dir, replacements)
+            .catch(err => {
+                if (process.env.NODE_ENV == "development")
+                    console.log(getDate(Date.now()), err.message)
+                return reject(raise("S01", 500))
+            })
+            .then(result => resolve(null))
+    })
 }
 
 /**
  * Sends confirmation email to the newly created user
- * @param {*} req 
- * @param {*} res 
+ * @param {Object} userInfo
+ * @returns {Promise<null>}
  */
-function sendConfirmationEmail(req, res) {
+function sendConfirmationEmail(userInfo) {
+    return new Promise(async (resolve, reject) => {
+        // Path to html
+        let dir = "/../templates/verify.html"
 
-    // Path to html
-    let dir = "/../templates/verify.html"
+        // Email config
+        let config = {
+            from: process.env.EMAIL,
+            to: userInfo.email,
+            subject: "RE: Account Verification"
+        }
 
-    // Email config
-    let config = {
-        from: process.env.EMAIL,
-        to: req.body.email,
-        subject: "RE: Account Verification"
-    }
+        // Replacements to the html file
+        let replacements = {
+            email: userInfo.email,
+            confirmationCode: userInfo.code,
+            emailSupport: "support@uniconapp.com"
+        }
 
-    // Replacements to the html file
-    let replacements = {
-        email: req.body.email,
-        confirmationCode: req.body.confirmationCode,
-        confirmationLink: `http://localhost:3001/register/verify/${req.body.uuid}/${req.body.confirmationCode}`,
-        emailSupport: "support@uniconapp.com"
-    }
-
-    // Send email
-    send(config.from, config.to, config.subject, dir, replacements)
-
-    res.status(200).send({ uuid: req.body.uuid })
+        // Send email
+        await send(config.from, config.to, config.subject, dir, replacements)
+            .catch(err => {
+                if (process.env.NODE_ENV == "development")
+                    console.log(getDate(Date.now()), err.message)
+                return reject(raise("S01", 500))
+            })
+            .then(result => resolve(null))
+    })
 }
 
 /**
- * Generates a new code and resends it to user
- * @param {*} req 
- * @param {*} res 
+ * Generates a new code and resends it to user. Accepts email only.
+ * @param {Object} userInfo
+ * @returns {Promise<null>}
  */
-async function resendConfirmationEmail(req, res) {
-    const user = await User.findOne({ _id: req.body.uuid })
+function resendConfirmationEmail(userInfo) {
+    return new Promise(async (resolve, reject) => {
+        const user = await User.findOne({ email: userInfo.email })
 
-    // Check for user
-    if (user == null) {
-        const error = new Error("User does not exist.")
-        error.code = "404"
-        throw error
-    }
-
-    // Generates a new code
-    const newCode = generateKey(3).toLowerCase()
-
-    user.confirmationCode = newCode
-    await user.save((err, result) => {
-        if (err) {
-            const error = new Error("Error saving to database.")
-            error.code = "500"
-            throw error
+        // Check for user
+        if (user == null) {
+            if (process.env.NODE_ENV == "development")
+                console.log(getDate(Date.now()), "User does not exists.")
+            return reject(raise("E03", 404))
         }
-    })
 
-    req.body.email = user.email
-    req.body.confirmationCode = newCode
-    sendConfirmationEmail(req, res)
-        .catch(err => {
+        // Check if already verified
+        if (user.status === "Active") {
+            if (process.env.NODE_ENV == "development")
+                console.log(getDate(Date.now()), "User is already verified.")
+            return reject(raise("E05", 409))
+        }
+
+        // Generates a new code
+        const newCode = generateKey(3).toLowerCase()
+
+        user.confirmationCode = newCode
+        user.save((err, result) => {
             if (err) {
-                const error = new Error(err.message)
-                error.code = "500"
-                throw error
+                if (process.env.NODE_ENV == "development")
+                    console.log(getDate(Date.now()), err.message)
+                return reject(raise("S01", 500))
             }
         })
+
+        // Resends confirmation email
+        userInfo.code = newCode
+        sendConfirmationEmail(userInfo)
+            .catch(err => {
+                if (err) {
+                    if (process.env.NODE_ENV == "development")
+                        console.log(getDate(Date.now()), err.message)
+                    return reject(raise("S01", 500))
+                }
+            })
+            .then(result => resolve(null))
+    })
 }
 
 // Exports
